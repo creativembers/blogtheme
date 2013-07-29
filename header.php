9a9a513b0eed7ca879f472ff35ba4a1a09d09305
Amanda Kremser <amanda@creativembers.com>
2013-07-29 11:49:24 -0700

    Add logo, begin laying out header

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="http://fonts.googleapis.com/css?family=Artifika" rel="stylesheet" type="text/css">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,800italic,400,700,800|Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 
 <?php wp_head(); ?>
 </head>
@@ -24,19 +25,21 @@
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description hide"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+		<div class="headercontainer clear">
+			<div class="site-branding">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description hide"><?php bloginfo( 'description' ); ?></h2>
+			</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'creativembers' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'creativembers' ); ?>"><?php _e( 'Skip to content', 'creativembers' ); ?></a></div>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<h1 class="menu-toggle"><?php _e( 'Menu', 'creativembers' ); ?></h1>
+				<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'creativembers' ); ?>"><?php _e( 'Skip to content', 'creativembers' ); ?></a></div>
 
-			<?php $walker = new Menu_With_Description; ?>
+				<?php $walker = new Menu_With_Description; ?>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'walker' => $walker ) ); ?>
-		</nav><!-- #site-navigation -->
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'walker' => $walker ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>	
 	</header><!-- #masthead -->
 
 	<div id="main" class="site-main">
