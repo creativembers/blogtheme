cc40363b34401bbcd2e58ced2ed69d2cd42fb242
Amanda Kremser <amanda@creativembers.com>
2013-07-29 21:48:08 -0700

    Header Adjustments, vertical centering

--- header.php
+++ header.php
@@ -24,22 +24,21 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<div class="headercontainer clear">
-			<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description hide"><?php bloginfo( 'description' ); ?></h2>
-			</div>
+		<header id="masthead" class="site-header" role="banner">
+			<div class="headercontainer clear">
+				<div class="site-branding">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description hide"><?php bloginfo( 'description' ); ?></h2>
+				</div>
 
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<h1 class="menu-toggle"><?php _e( 'Menu', 'creativembers' ); ?></h1>
-				<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'creativembers' ); ?>"><?php _e( 'Skip to content', 'creativembers' ); ?></a></div>
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<h1 class="menu-toggle"><?php _e( 'Menu', 'creativembers' ); ?></h1>
+					<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'creativembers' ); ?>"><?php _e( 'Skip to content', 'creativembers' ); ?></a></div>
 
-				<?php $walker = new Menu_With_Description; ?>
+					<?php $walker = new Menu_With_Description; ?>
 
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'walker' => $walker ) ); ?>
-			</nav><!-- #site-navigation -->
-		</div>	
-	</header><!-- #masthead -->
-
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'walker' => $walker ) ); ?>
+				</nav><!-- #site-navigation -->
+			</div>	
+		</header><!-- #masthead -->
 	<div id="main" class="site-main">
